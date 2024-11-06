96cd6f186b04461a2d7adda5090eba13d254cb93
liadaboiko <Vladyslava.Boiko.RI.2024@lpnu.ua>
2024-11-06 11:28:30 +0200

    Add files via upload

---
+++ UnitTest.lab6.1.rec.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestlab61rec
+{
+	TEST_CLASS(UnitTestlab61rec)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
